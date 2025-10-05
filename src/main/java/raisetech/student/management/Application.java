414197931e5694a696e77c05977da3a9bd5ee6f5
suzuki <suzukitakuro@icloud.com>
2025-10-05 17:16:49 +0900

    提出後修正コミット

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.stream.Collector;
+
 
 @SpringBootApplication
 @RestController
 public class Application {
 
 
-    private Map<String, Integer> dnamemberinfo = new HashMap<>();
+    private final Map<String, Integer> dnamemberinfo = new HashMap<>();
 
     {
         dnamemberinfo.put("蛯名", 28);
@@ -22,40 +22,23 @@ public class Application {
         dnamemberinfo.put("オースティン", 34);
         dnamemberinfo.put("佐野", 31);
         dnamemberinfo.put("ジャクソン", 29);
-        dnamemberinfo.put("山本",27);
-        dnamemberinfo.put("東",29);
+        dnamemberinfo.put("山本", 27);
+        dnamemberinfo.put("東", 29);
     }
 
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
     }
-
     @GetMapping("/dnamemberinfo")
-    public Map<String, Integer> getMemberinfo() {
+    public Map<String, Integer> getdnamemberinfo() {
         return dnamemberinfo;
     }
 
-    @GetMapping("/dnamembername")
-    public Set<String> getdnamembername() {
-        return dnamemberinfo.keySet();
-    }
-
-    @GetMapping("/dnamemberage")
-    public Collection<Integer> getdnamemberage() {
-        return dnamemberinfo.values();
-    }
-
     @PostMapping("/dnamemberinfo")
-    public void setmemberinfo(@RequestBody Map<String, Integer> newmember) {
+    public void setdnamemberinfo(@RequestBody Map<String, Integer> newmember) {
         dnamemberinfo.putAll(newmember);
     }
-
-    @PostMapping("/dnamembername")
-    public void setmemberinfo(@RequestBody String key) {
-        dnamemberinfo.put(key,null);
-    }
-
 
 
 }
