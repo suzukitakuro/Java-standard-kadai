c0bd5b8700d93e3ff4023efd2d7231bfab0f6681
suzuki <suzukitakuro@icloud.com>
2026-01-25 15:50:00 +0900

    スタンダード22回課題

--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -1,5 +1,8 @@
 package raisetech.student.management.data;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,14 +15,22 @@ import lombok.Setter;
 public class Student {
 
 
+
     private String id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String kanaName;
+    @NotBlank
     private String nickname;
+    @NotBlank
     private String email;
+    @NotBlank
     private String area;
     private int age;
+    @NotBlank
     private String sex;
+
     private String remark;
     private boolean isDeleted;
 }
