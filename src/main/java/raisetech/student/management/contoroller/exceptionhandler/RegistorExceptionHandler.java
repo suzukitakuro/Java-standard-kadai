d1a4546db36f01a85f31fde8e91bd656ff96801f
suzuki <suzukitakuro@icloud.com>
2026-02-11 10:24:04 +0900

    スタンダード23回課題

--- src/main/java/raisetech/student/management/contoroller/exceptionhandler/RegistorExceptionHandler.java
+++ src/main/java/raisetech/student/management/contoroller/exceptionhandler/RegistorExceptionHandler.java
@@ -4,14 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import raisetech.student.management.exception.RegistorTestException;
 import raisetech.student.management.exception.TestException;
 import raisetech.student.management.exception.TestException;
 
 @RestControllerAdvice
 public class RegistorExceptionHandler {
 
-    @ExceptionHandler(TestException.class)
-    public ResponseEntity<String> handleRegistorException(TestException ex) {
+    @ExceptionHandler(RegistorTestException.class)
+    public ResponseEntity<String> handleRegistorException(RegistorTestException ex) {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
 
